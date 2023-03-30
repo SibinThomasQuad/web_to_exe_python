12a84259690dafc72cdd37f6fa3c1a002329f182
Sibin Thomas <54390036+SibinThomasQuad@users.noreply.github.com>
2023-03-30 18:21:24 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+# Import the required libraries
+from tkinter import *
+import webview
+
+# Create an instance of tkinter frame or window
+win = Tk()
+
+# Set the size of the window
+win.geometry("700x350")
+# Create a GUI window to view the HTML content
+webview.create_window('tutorialspoint', 'we_url')
+webview.start()
